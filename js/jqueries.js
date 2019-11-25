9e58ea50b592f195cd4bb5bdaccd9dc96f2154dd
hbFree <ih_beldjoudi@esi.dz>
2019-11-25 02:08:21 +0100

    [IDE->PYCHARM]: code formats files with pycharm

--- js/jqueries.js
+++ js/jqueries.js
@@ -1,32 +1,32 @@
-$(document).ready(function(){
-	
-	if(navigator.appVersion.indexOf("Edge") != -1){
-		$('body').css({zoom: "85%"});
-	}
-	
-	var quoteIsVisible = false;
-	var quoteToogleFade = () => {
-		if(quoteIsVisible)
-			$(".quote").fadeOut(700);
-		else $(".quote").fadeIn(700);
-		quoteIsVisible = !quoteIsVisible; 
-	};
-	quoteToogleFade();
-	
-	$("#quote-slider").click(() => {
-		quoteToogleFade();
-	});
-	
-	$('#tifibtn').click( () => {
-		$('.latinbox').fadeOut(500, ()=>$('.tifibox').show());
+$(document).ready(function () {
 
-	});
-	$('#latinbtn').click( () => {
-		$('.tifibox').fadeOut(500, ()=> $('.latinbox').show());
-	});
-	$("#from-lng")
-		.focus() // set initial focus
-		.on('blur', function () { // on blur
-		$(this).focus(); // return focus	
-		});
+    if (navigator.appVersion.indexOf("Edge") != -1) {
+        $('body').css({zoom: "85%"});
+    }
+
+    var quoteIsVisible = false;
+    var quoteToogleFade = () => {
+        if (quoteIsVisible)
+            $(".quote").fadeOut(700);
+        else $(".quote").fadeIn(700);
+        quoteIsVisible = !quoteIsVisible;
+    };
+    quoteToogleFade();
+
+    $("#quote-slider").click(() => {
+        quoteToogleFade();
+    });
+
+    $('#tifibtn').click(() => {
+        $('.latinbox').fadeOut(500, () => $('.tifibox').show());
+
+    });
+    $('#latinbtn').click(() => {
+        $('.tifibox').fadeOut(500, () => $('.latinbox').show());
+    });
+    $("#from-lng")
+        .focus() // set initial focus
+        .on('blur', function () { // on blur
+            $(this).focus(); // return focus
+        });
 });
--- js/view.js
+++ js/view.js
@@ -1,24 +1,23 @@
-
 Vue.component('txtinput',
-	{
-		template:
-		`<div class = 'input-txt-div'>
+    {
+        template:
+            `<div class = 'input-txt-div'>
 				<div class = "input-legend" style = "padding-bottom:5px;"> 
 				<img class = "rounded float-left" v-bind:src = "flag" style="width:30px;height:15px;margin-top:4px;"/>
 				<span style="padding-left:10px;">{{ lng }}</span>
 				</div>
 				<textarea style="padding:10px;" class = "input" spellcheck="false" v-bind:id="lngid" v-bind:disabled="isdisabled"> </textarea>
 			</div>`
-		,
-		props: ['lng', 'flag', 'lngid', 'isdisabled'],
+        ,
+        props: ['lng', 'flag', 'lngid', 'isdisabled'],
 
-	}
+    }
 );
 
 Vue.component('alpha',
-	{
-		template:
-		`<div class="couple-alphas-holder">
+    {
+        template:
+            `<div class="couple-alphas-holder">
 			<div class="alpha-box, latinbox" >
 				<button class="berber-alpha latinalpha" v-on:click = "appendAlpha(berberalpha)">
 				{{ berberalpha }}
@@ -31,47 +30,44 @@ Vue.component('alpha',
 			</div>
 		</div>
 		`
-		,
-		props: ['berberalpha', 'tifinaghalpha']
-		,
-		methods:
-		{
-			appendAlpha(c)
-			{
-				let inner = $("#from-lng").val();
-				$("#from-lng").val(inner + c);
-			}
-		}
-	}
+        ,
+        props: ['berberalpha', 'tifinaghalpha']
+        ,
+        methods:
+            {
+                appendAlpha(c) {
+                    let inner = $("#from-lng").val();
+                    $("#from-lng").val(inner + c);
+                }
+            }
+    }
 );
 
-var alphasApp = new Vue ({
-	el: "#MainApp",
-	data: {
-		tifinaghAlphas: "ⴰⴱⴳⴷⴹⴻⴼⴽⵀⵃⵄⵅⵇⵉⵊⵍⵎⵏⵓⵔⵕⵖⵙⵚⵛⵜⵟⵡⵢⵣⵥⵯⵞⴶ",
-		berberAlphas: "abgdḌefkhḤɛxqijlmnurṚɣsṢctṬwyzẒwčǧ",
-		fromLng: "Tamaziɣ",
-		toLng: "English",
-		fromFlag: 'img/berber-flag.png',
-		toFlag: 'img/uk-flag.png',
-	}
-	,
-	methods:
-	{
-		swapLngs()
-		{
-			let tmp = this.fromLng, tmpf = this.fromFlag;
-			this.fromLng = this.toLng;
-			this.fromFlag = this.toFlag;
-			this.toLng = tmp;
-			this.toFlag = tmpf;
-		}
-		,
-		emptyAreas()
-		{
-			$(".input").val('');
-		}
-	}
+var alphasApp = new Vue({
+    el: "#MainApp",
+    data: {
+        tifinaghAlphas: "ⴰⴱⴳⴷⴹⴻⴼⴽⵀⵃⵄⵅⵇⵉⵊⵍⵎⵏⵓⵔⵕⵖⵙⵚⵛⵜⵟⵡⵢⵣⵥⵯⵞⴶ",
+        berberAlphas: "abgdḌefkhḤɛxqijlmnurṚɣsṢctṬwyzẒwčǧ",
+        fromLng: "Tamaziɣ",
+        toLng: "English",
+        fromFlag: 'img/berber-flag.png',
+        toFlag: 'img/uk-flag.png',
+    }
+    ,
+    methods:
+        {
+            swapLngs() {
+                let tmp = this.fromLng, tmpf = this.fromFlag;
+                this.fromLng = this.toLng;
+                this.fromFlag = this.toFlag;
+                this.toLng = tmp;
+                this.toFlag = tmpf;
+            }
+            ,
+            emptyAreas() {
+                $(".input").val('');
+            }
+        }
 });
 
 
